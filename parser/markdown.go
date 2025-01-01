8584fc0a01ac6f4b3fdcbba8f9b7329b568c69f6
Akash Jain <ajcodes.tech@gmail.com>
2025-01-01 19:09:26 +0000

    template

---
+++ parser/markdown.go
@@ -0,0 +1,7 @@
+package parser
+
+// This functions takes an markdown string as an input and create an equivalent html file.
+func MarkDownParser() string {
+	// output directory
+	// project root dir -> html -> <file>.html
+}
